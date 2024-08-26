0ef1213ff6ca832a840c355b1b74478c7169ad23
jadg13 <jadg13@hotmail.com>
2024-08-26 10:40:18 -0600

    Clase 26/08/2024

---
+++ Sesion3/Sesion3/Program.cs
@@ -0,0 +1,22 @@
+﻿using Sesion3.modelos;
+
+namespace Sesion3
+{
+    internal class Program
+    {
+        /* Calcular la edad de una persona y decir si es mayor o menor de edad*/
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechaNac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de Nacimiento: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre;
+            persona.FechaNac = fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+        }
+    }
+}
---
+++ Sesion3/Sesion3/modelos/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion3.modelos
+{
+    internal class Persona
+    {
+        public Persona() { }
+
+        public string Nombre { get; set; }
+
+        public DateTime FechaNac { get; set; }
+
+        public int CalcularEdad()
+        {
+            int edad = 0;
+            try
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Ocurio un error: {ex.Message}");
+            }
+            return edad;
+        }
+
+        public string EvaluarEdad()
+        {
+            if (CalcularEdad() >= 18) return "Mayor de edad";
+            return "Menor de edad";
+        }
+
+    }
+}
